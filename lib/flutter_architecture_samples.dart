493c11e072e96eed783287c95868464722e02867
Bradley Anderson <brad@theappguy.guru>
2019-02-02 17:15:54 -0800

    Initial

---
+++ lib/flutter_architecture_samples.dart
@@ -0,0 +1,12 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+/// The core logic and presentation code that each app should feel free to use
+/// as a base for .
+library flutter_architecture_samples;
+
+export 'keys.dart';
+export 'localization.dart';
+export 'routes.dart';
+export 'theme.dart';
---
+++ lib/presentation/home_screen.dart
@@ -0,0 +1,44 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:fire_redux_sample/containers/active_tab.dart';
+import 'package:fire_redux_sample/containers/extra_actions_container.dart';
+import 'package:fire_redux_sample/containers/filter_selector.dart';
+import 'package:fire_redux_sample/containers/filtered_todos.dart';
+import 'package:fire_redux_sample/containers/stats.dart';
+import 'package:fire_redux_sample/containers/tab_selector.dart';
+import 'package:fire_redux_sample/flutter_architecture_samples.dart';
+import 'package:fire_redux_sample/models/models.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen() : super(key: ArchSampleKeys.homeScreen);
+
+  @override
+  Widget build(BuildContext context) {
+    return ActiveTab(
+      builder: (BuildContext context, AppTab activeTab) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Untangeled"),
+            actions: [
+              FilterSelector(visible: activeTab == AppTab.todos),
+              ExtraActionsContainer(),
+            ],
+          ),
+          body: activeTab == AppTab.todos ? FilteredTodos() : Stats(),
+          floatingActionButton: FloatingActionButton(
+            key: ArchSampleKeys.addTodoFab,
+            onPressed: () {
+              Navigator.pushNamed(context, ArchSampleRoutes.addTodo);
+            },
+            child: Icon(Icons.add),
+            tooltip: "Add Todos",
+          ),
+          bottomNavigationBar: TabSelector(),
+        );
+      },
+    );
+  }
+}
